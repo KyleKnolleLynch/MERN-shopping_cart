df63ffc21a604b4c4f16e9b0266669ee1703c83c
Kyle Lynch <kylelynch104@gmail.com>
2018-08-16 09:18:48 -0700

    complete_mern_app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const items = require('./routes/api/items');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -9,16 +9,26 @@ const app = express();
 app.use(express.json());
 
 //  DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use("/api/items", items);
+
+// Serve static assets if in Production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client.build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
